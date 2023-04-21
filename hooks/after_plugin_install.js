f63e9ffb549f6598675c6102aac317a940270691
miladesign <rezagah.milad@yahoo.com>
2023-04-21 21:04:18 +0330

    Fixed for android 12

--- hooks/after_plugin_install.js
+++ hooks/after_plugin_install.js
@@ -13,7 +13,7 @@ module.exports = function(context) {
 			if(data.indexOf('com.farsitel.bazaar.permission.PAY_THROUGH_BAZAAR') > 0) {
 				console.log("Selected market is Bazaar.");
 				data = data.replace("<application", '\r\n\t<queries>\r\n\t\t<package android:name="com.farsitel.bazaar" />\r\n\t\t<intent>\r\n\t\t\t<action android:name="ir.cafebazaar.pardakht.InAppBillingService.BIND" />\r\n\t\t\t<data android:mimeType="*/*" />\r\n\t\t</intent>\r\n\t</queries>\r\n\r\n\t<application');
-				var result = data.replace("</application>", '\r\n\t\t<receiver android:name="vinoos.cordova.iap.util.IABReceiver">\r\n\t\t\t<intent-filter>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.ping"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.purchase"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.billingSupport"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.consume"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.getPurchase"/>\r\n\t\t\t</intent-filter>\r\n\t\t</receiver>\r\n\t</application>');
+				var result = data.replace("</application>", '\r\n\t\t<receiver android:name="vinoos.cordova.iap.util.IABReceiver" android:exported="false">\r\n\t\t\t<intent-filter>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.ping"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.purchase"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.billingSupport"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.consume"/>\r\n\t\t\t\t<action android:name="com.farsitel.bazaar.getPurchase"/>\r\n\t\t\t</intent-filter>\r\n\t\t</receiver>\r\n\t</application>');
 				fs.writeFile(manifestFile, result, 'utf8', function (err) {
 					if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
 				});
@@ -21,7 +21,7 @@ module.exports = function(context) {
 			} else if(data.indexOf('ir.mservices.market.BILLING') > 0) {
 				console.log("Selected market is Myket.");
 				data = data.replace("<application", '\r\n\t<queries>\r\n\t\t<package android:name="ir.mservices.market" />\r\n\t\t<intent>\r\n\t\t\t<action android:name="ir.mservices.market.InAppBillingService.BIND" />\r\n\t\t\t<data android:mimeType="*/*" />\r\n\t\t</intent>\r\n\t</queries>\r\n\r\n\t<application');
-				var result = data.replace("</application>", '\r\n\t\t<receiver android:name="vinoos.cordova.iap.util.IABReceiver">\r\n\t\t\t<intent-filter>\r\n\t\t\t\t<action android:name="ir.mservices.market.ping"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.purchase"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.billingSupport"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.consume"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.getPurchase"/>\r\n\t\t\t</intent-filter>\r\n\t\t</receiver>\r\n\t</application>');
+				var result = data.replace("</application>", '\r\n\t\t<receiver android:name="vinoos.cordova.iap.util.IABReceiver" android:exported="false">\r\n\t\t\t<intent-filter>\r\n\t\t\t\t<action android:name="ir.mservices.market.ping"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.purchase"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.billingSupport"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.consume"/>\r\n\t\t\t\t<action android:name="ir.mservices.market.getPurchase"/>\r\n\t\t\t</intent-filter>\r\n\t\t</receiver>\r\n\t</application>');
 				fs.writeFile(manifestFile, result, 'utf8', function (err) {
 					if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
 				});
